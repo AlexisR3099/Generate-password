379fa47711e197df13204ce119b447369593a102
Slimeball <alexramo2330@gmail.com>
2022-08-12 13:52:36 -0500

    prompt-results

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var lowerCase
 var upperCase;
 var numValue;
 var specialCharacters;
+var option;
 
 // variables/characters
 character = ['!', '#', '$', '%', '%', '*', '(', ')', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']'];
@@ -33,4 +34,69 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function() {
+  var generatePassword = function() {
+    enter = parseInt(prompt('How many characters in your password?'));
+
+    if(!enter) {
+      alert('Please enter a value!')
+    } else if (enter < 8 || enter > 128) {
+      enter = parseInt(prompt('Length must be between 8-128 characters'));
+    } else {
+
+      lowerCase = confirm("Allow 'lowercase' letters in your password?");
+      upperCase = confirm("Allow 'uppercase' letters in your password?");
+      numValue = confirm("Allow 'numbers' to be in your password?");
+      specialCharacters = confirm("Allow 'special characters' to be in your password");
+    };
+    if (!lowerCase && !upperCase && !numValue && !specialCharacters) {
+      option = alert("Choose at least one criteria!");
+    }
+    // all options
+    else if (lowerCase && upperCase && numValue && specialCharacters) {
+      option = character.concat(number, letter, letterCaps);
+    }
+    // 3/4 options picked
+    else if (lowerCase && upperCase && numValue) {
+      option = letter.concat(letterCaps, number);
+    }
+    else if (lowerCase && upperCase && specialCharacters) {
+      option = letter.concat(letterCaps, character);
+    }
+    else if (upperCase && numValue && specialCharacters) {
+      option = number.concat(letterCaps, number);
+    }
+    // 2/4 options picked 
+    else if (lowerCase && upperCase) {
+      option = letter.concat(letterCaps);
+    }
+    else if (lowerCase && numValue) {
+      option = letter.concat(number);
+    }
+    else if (lowerCase && specialCharacters) {
+      option = letter.concat(character);
+    }
+    else if (upperCase && numValue) {
+      option = letterCaps.concat(number);
+    }
+    else if (upperCase && specialCharacters) {
+      option = letterCaps.concat(character);
+    }
+    else if (numValue && specialCharacters) {
+      option = number.concat(character);
+    }
+    // 1 option picked
+    else if (lowerCase) {
+      option = letter;
+    }
+    else if (upperCase) {
+      option = letterCaps;
+    }
+    else if (numValue) {
+      option = number;
+    }
+    else if (specialCharacters) {
+      option = character;
+    }
+  }
+});
