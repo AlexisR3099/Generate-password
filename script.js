0d89622d58811963b6b70ae03327f5fa81881c2c
Slimeball <alexramo2330@gmail.com>
2022-08-12 13:01:37 -0500

    character-arrays

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 // Assignment code here
+var enter;
+// prompts
+var characterAmount;
+var lowerCase
+var upperCase;
+var numValue;
+var specialCharacters;
+
+// variables/characters
+character = ['!', '#', '$', '%', '%', '*', '(', ')', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']'];
+number = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// make capital letters
+function makeUpperCase(x) {
+  return x.toUpperCase();
+};
+
+letterCaps = letter.map(makeUpperCase);
 
 
 // Get references to the #generate element
